bad85b490487ef2ff048ed08ceebdfdeed6439e4
S V Ananthakrishna <s.vananthakrishna@students.iiserpune.ac.in>
2026-09-10 17:59:41 +0200

    Add files via upload

---
+++ selecting_genes.r
@@ -0,0 +1,27 @@
+library(dplyr)
+library(stringr)
+
+setwd(".")
+
+#recovering all the hypoxia genes
+hypoxia_ma <- read.csv2(file = "microarrays_hypoxia.csv")
+hypoxia_rnaseq <- read.csv(file = "rnaseq_hypoxia.csv",sep=";",row.names = NULL)%>% filter(str_starts(row.names,"C"))
+
+#dataframe with the intersection of genes identified in both techniques
+hypoxia_inter <- hypoxia_rnaseq%>%filter(row.names%in%rownames(hypoxia_ma)) %>% arrange(-log2FoldChange)
+
+#create a rank for FC
+hypoxia_inter <- hypoxia_inter %>% mutate(rank_FC=c(1:32))
+
+#create a rank for adjpvalue
+hypoxia_inter <- hypoxia_inter %>% arrange(padj)
+hypoxia_inter <- hypoxia_inter %>% mutate(rank_padj=c(1:32))
+
+#calculate the mean of the two ranks
+hypoxia_inter$mean_rank <- (hypoxia_inter$rank_FC+hypoxia_inter$rank_padj)/2
+
+#list of genes arranged by the new index
+hypoxia_inter <- hypoxia_inter %>% arrange(mean_rank)
+
+#---- Reproducibility & session info ----
+sessionInfo()
